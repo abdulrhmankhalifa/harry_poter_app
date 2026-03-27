e1b97c0ea9982239207dfadd92768ab798063ba0
Abdulrhman Khalifa <abdulrhman@example.com>
2026-03-27 15:02:24 +0200

    add searched filter

--- lib/ui/screens/characters_screen.dart
+++ lib/ui/screens/characters_screen.dart
@@ -15,34 +15,93 @@ class CharactersScreen extends StatefulWidget {
 
 class _CharactersScreenState extends State<CharactersScreen> {
   late List<Character> allCharacters;
+  late List<Character> searchedCharacters;
+  bool isSearching = false;
+  final searchTextController = TextEditingController();
+
+  Widget buildSearchField() {
+    return TextField(
+      controller: searchTextController,
+      cursorColor: MyColors.characterTextColor,
+      decoration: InputDecoration(
+        hintText: 'Search for a character',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: MyColors.characterTextColor, fontSize: 18),
+      ),
+      style: TextStyle(color: MyColors.characterTextColor, fontSize: 18),
+      onChanged: (searchedCharacter) {
+        addSearchedForItemsToSearchedList(searchedCharacter);
+      },
+    );
+  }
+
+  void addSearchedForItemsToSearchedList(String searchedCharacter) {
+    searchedCharacters = allCharacters
+        .where(
+          (character) => character.name.toLowerCase().startsWith(
+            searchedCharacter.toLowerCase(),
+          ),
+        )
+        .toList();
+    setState(() {});
+  }
+
+  List<Widget> buildAppBarActions() {
+    if (isSearching) {
+      return [
+        IconButton(
+          onPressed: () {
+            clearSearch();
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.clear, color: MyColors.characterTextColor),
+        ),
+      ];
+    } else {
+      return [
+        IconButton(
+          onPressed: startSearching,
+          icon: Icon(Icons.search, color: MyColors.characterTextColor),
+        ),
+      ];
+    }
+  }
+
+  void startSearching() {
+    ModalRoute.of(
+      context,
+    )!.addLocalHistoryEntry(LocalHistoryEntry(onRemove: stopSearching));
+    setState(() {
+      isSearching = true;
+    });
+  }
+
+  void stopSearching() {
+    clearSearch();
+    setState(() {
+      isSearching = false;
+    });
+  }
+
+  void clearSearch() {
+    setState(() {
+      searchTextController.clear();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    allCharacters = BlocProvider.of<CharactersCubit>(
-      context,
-    ).getAllCharacters();
+    BlocProvider.of<CharactersCubit>(context).getAllCharacters();
   }
 
   //بتعرض الاب بار بتاع الشاشه
-  PreferredSizeWidget buildAppBar() {
-    return AppBar(
-      backgroundColor: MyColors.backgroundColor,
-      title: Text(
-        'Characters',
-        style: MyFonts.sourceCodeProBold.copyWith(
-          color: MyColors.characterTextColor,
-          fontSize: 24,
-        ),
-      ),
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(1.0),
-        child: Divider(
-          color: MyColors.characterTextColor,
-          thickness: 2,
-          indent: 8,
-          endIndent: 8,
-        ),
+  Widget buildAppBarTitle() {
+    return Text(
+      'Characters',
+      style: MyFonts.sourceCodeProBold.copyWith(
+        color: MyColors.characterTextColor,
+        fontSize: 24,
       ),
     );
   }
@@ -65,7 +124,23 @@ class _CharactersScreenState extends State<CharactersScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.backgroundColor,
-      appBar: buildAppBar(),
+      appBar: AppBar(
+        leading: isSearching
+            ? const BackButton(color: MyColors.characterTextColor)
+            : null,
+        backgroundColor: MyColors.backgroundColor,
+        title: isSearching ? buildSearchField() : buildAppBarTitle(),
+        actions: buildAppBarActions(),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Divider(
+            color: MyColors.characterTextColor,
+            thickness: 2,
+            indent: 8,
+            endIndent: 8,
+          ),
+        ),
+      ),
       body: buildCharactersWidget(),
     );
   }
@@ -98,10 +173,16 @@ class _CharactersScreenState extends State<CharactersScreen> {
       ),
       shrinkWrap: true,
       physics: const ClampingScrollPhysics(),
-      itemCount: allCharacters.length,
+      itemCount: searchTextController.text.isEmpty
+          ? allCharacters.length
+          : searchedCharacters.length,
       padding: EdgeInsets.zero,
       itemBuilder: (context, index) {
-        return CharacterItem(character: allCharacters[index]);
+        return CharacterItem(
+          character: searchTextController.text.isEmpty
+              ? allCharacters[index]
+              : searchedCharacters[index],
+        );
       },
     );
   }
